88a5ba98d95d64466fd663d174f70f17c33d1467
LeoLuxo <leo@gamboa.eu>
2023-04-22 22:11:46 +0200

    Ball

--- scene.glsl
+++ scene.glsl
@@ -6,31 +6,32 @@
 
 Surface sdfScene(vec3 p, float time)
 {
+	
+	// float ref = sin(time) / 2.0 + 0.5;
+	
 	Surface sphere = Surface(
-		sdfSphere(p - vec3(0.0, 2.0, 0.0), 1.0),
+		sdfSphere(p - vec3(sin(-time * 1.0), 2.0, 0.0), 1.0),
 		vec3(0.0),
 		0.1, 8.0,
 		0.1,
-		1.0, 1.6
+		0.9, 1.6
 	);
 	
-	Surface sphere2a = Surface(
-		sdfSphere(p - vec3(2.02, 2.0, 0.0), 2.0),
-		vec3(0.0),
+	Surface red = Surface(
+		sdfSphere(p - vec3(sin(time * 0.5) * 1.0, 2.0, 0.0), 0.4),
+		vec3(1.0, 0.0, 0.0),
 		0.1, 8.0,
-		0.1,
-		1.0, 1.6
+		0.9,
+		0.1, 1.6
 	);
 	
-	Surface sphere2b = Surface(
-		sdfSphere(p - vec3(-2.02, 2.0, 0.0), 2.0),
-		vec3(0.0),
+	Surface red2 = Surface(
+		sdfSphere(p - vec3(sin(time * 0.5) * 1.0, 2.0, 0.0), 0.5),
+		vec3(1.0, 0.0, 0.0),
 		0.1, 8.0,
-		0.1,
-		1.0, 1.6
+		0.9,
+		0.1, 1.6
 	);
-	
-	float ref = sin(time) / 2.0 + 0.5;
 	
 	Surface plane;
 	if (fract(p.x*0.5) < 0.5 != fract(p.z*0.5) < 0.5)
@@ -39,8 +40,8 @@ Surface sdfScene(vec3 p, float time)
 		plane = Surface(sdfFloor(p, 0.0), vec3(0.1), 0.1, 8.0, 0.0, 0.0, 1.0);
 	
 	Surface d = sphere;
-	d = blendDiff(d, sphere2a);
-	d = blendDiff(d, sphere2b);
+	d = blendDiff(d, red2);
+	d = blendSMin(d, red, 0.5);
 	d = blendMin(d, plane);
 	
 	// Surface d = plane;
