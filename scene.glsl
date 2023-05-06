60580daeba21310a5b385627ca42953d1659581f
LeoLuxo <leo@gamboa.eu>
2023-05-06 10:38:24 +0200

    screenshots for report

--- scene.glsl
+++ scene.glsl
@@ -6,8 +6,10 @@
 
 Surface sdfScene(vec3 p, float time)
 {
+	float t = sin(clamp(mod(time * 0.05, 1.0), 0.0, 0.5) * 3.1415) * 5.5;
+	
 	Surface sphere = Surface(
-		sdfSphere(p - vec3(0.0, 0.0, 0.0), 1.3),
+		sdfSphere(p - vec3(0.0, 5.0 - t, 0.0), 1.3),
 		vec3(1.0, 0.0, 0.0),
 		0.0, 8.0,
 		0.0,
@@ -15,7 +17,7 @@ Surface sdfScene(vec3 p, float time)
 	);
 	
 	Surface box = Surface(
-		sdfBox(p - vec3(0.0, 0.0, 0.0), vec3(2.0)),
+		sdfBox(p - vec3(0.0, -0.5, 0.0), vec3(2.0)),
 		vec3(0.0, 0.0, 1.0),
 		0.0, 8.0,
 		0.0,
